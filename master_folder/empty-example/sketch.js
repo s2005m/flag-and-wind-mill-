876efb559e0a0741202d710fc78be2782bca4487
s2005m <32396330+s2005m@users.noreply.github.com>
2017-10-27 21:41:18 +0530

    Add files via upload

---
+++ master_folder/empty-example/sketch.js
@@ -0,0 +1,22 @@
+function setup(){
+    createCanvas(800,500);
+}
+
+
+function draw(){
+
+  // flag
+  background(120,50,50);
+  rect(200,200,10,200);
+  rect(200,100,180,100);
+
+
+  //windmill
+  line(600,200,600,400);
+  triangle(600,200,580,150,620,150);
+  triangle(600,200,650,180,650,220);
+  triangle(600,200,550,180   ,550,   220);
+
+
+
+}
